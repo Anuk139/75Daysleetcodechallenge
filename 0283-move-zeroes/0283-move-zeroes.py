c0473789f54b0376fdda2b315b595da8545f92a4
Anuk139 <anuk38940@gmail.com>
2026-03-21 18:38:35 +0530

    Time: 0 ms (100%), Space: 13.3 MB (92.98%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.py
+++ 0283-move-zeroes/0283-move-zeroes.py
@@ -4,9 +4,15 @@ class Solution(object):
         :type nums: List[int]
         :rtype: None Do not return anything, modify nums in-place instead.
         """
-        j = 0
+        if not nums:
+            return 
+        k = 0
+        for i, num in enumerate(nums):
+            if num != 0:
+                nums[k] = num
+                k += 1
+            
+        for i in range(k, len(nums)):
+            nums[i] = 0
 
-        for i in range(len(nums)):
-            if nums[i] != 0:
-                nums[j], nums[i] = nums[i], nums[j]
-                j += 1
+        
