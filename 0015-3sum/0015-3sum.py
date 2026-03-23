be02a7e58d3e83ee498ff1aca8af6c199297a9e6
Anuk139 <anuk38940@gmail.com>
2026-03-23 11:05:49 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0015-3sum/0015-3sum.py
+++ 0015-3sum/0015-3sum.py
@@ -7,7 +7,7 @@ class Solution(object):
         nums.sort()
         res = []
 
-        for i in range(len(nums) - 2):
+        for i in range(len(nums)):
             if i > 0 and nums[i] == nums[i-1]:
                 continue
             
